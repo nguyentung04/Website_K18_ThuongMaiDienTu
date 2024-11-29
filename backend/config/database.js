08417848cf634d9687a731111cb33c06e2ef151e
ThaiPhu2k4 <132972807+ThaiPhu2k4@users.noreply.github.com>
2024-11-29 12:33:41 +0700

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,3 +18,21 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',  // Thay đổi theo cấu hình của bạn
+//   user: 'root',       // Thay đổi theo cấu hình của bạn
+//   password: '',  // Thay đổi theo cấu hình của bạn
+//   database: 'mau' // Thay đổi theo cấu hình của bạn
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);  
+//     return;
+//   }
+//   console.log('connected as id ' + connection.threadId);
+// });
+
+// module.exports = connection;

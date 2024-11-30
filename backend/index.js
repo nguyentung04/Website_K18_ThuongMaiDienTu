a820fdbfff8daa0a90f324d822c21d9b98c015ad
QUY611804 <130846223+QUY611804@users.noreply.github.com>
2024-11-30 16:39:59 +0700

    cập nhật lại thanh toán momo

--- backend/index.js
+++ backend/index.js
@@ -128,61 +128,59 @@ app.listen(port, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var accessKey = "F8BBA842ECF85";
-var secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
+
 //payment
 app.post("/payment", async (req, res) => {
-  //https://developers.momo.vn/#/docs/en/aiov2/?id=payment-method
-  //parameters
+ //https://developers.momo.vn/#/docs/en/aiov2/?id=payment-method
+//parameters
+var accessKey = 'F8BBA842ECF85';
+var secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
+var orderInfo = req.body.orderInfo;
+var partnerCode = 'MOMO';
+var redirectUrl = "http://localhost:3001/formcheckout";
+var ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
+var requestType = "payWithMethod";
+var amount = req.body.amount;
+var orderId = partnerCode + new Date().getTime();
+var requestId = orderId;
+var extraData ='';
+var orderGroupId ='';
+var autoCapture =true;
+var lang = 'vi';
 
-  var orderInfo = req.body.orderInfo;
-  var partnerCode = "MOMO";
-  var redirectUrl = "http://localhost:3001/formcheckout";
-  var ipnUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
-  var requestType = "payWithMethod";
-  var amount = req.body.amount;
-  var orderId = partnerCode + new Date().getTime();
-  var requestId = orderId;
-  var extraData = "";
-  var paymentCode =
-    "T8Qii53fAXyUftPV3m9ysyRhEanUs9KlOPfHgpMR0ON50U10Bh+vZdpJU7VY4z+Z2y77fJHkoDc69scwwzLuW5MzeUKTwPo3ZMaB29imm6YulqnWfTkgzqRaion+EuD7FN9wZ4aXE1+mRt0gHsU193y+yxtRgpmY7SDMU9hCKoQtYyHsfFR5FUAOAKMdw2fzQqpToei3rnaYvZuYaxolprm9+/+WIETnPUDlxCYOiw7vPeaaYQQH0BF0TxyU3zu36ODx980rJvPAgtJzH1gUrlxcSS1HQeQ9ZaVM1eOK/jl8KJm6ijOwErHGbgf/hVymUQG65rHU2MWz9U8QUjvDWA==";
-  var orderGroupId = "";
-  var autoCapture = true;
-  var lang = "vi";
-
-  //before sign HMAC SHA256 with format
-  //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
-  var rawSignature = "accessKey=" + accessKey +"&amount=" + amount +"&extraData=" + extraData +"&ipnUrl=" + ipnUrl + "&orderId=" + orderId +"&orderInfo=" + orderInfo +"&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl +"&requestId=" +requestId +"&requestType=" +requestType;
-  //puts raw signature
-  console.log("--------------------RAW SIGNATURE----------------");
-  console.log(rawSignature);
-  //signature
-  const crypto = require("crypto");
-  var signature = crypto
-    .createHmac("sha256", secretKey)
+//before sign HMAC SHA256 with format
+//accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
+var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType;
+//puts raw signature
+console.log("--------------------RAW SIGNATURE----------------")
+console.log(rawSignature)
+//signature
+const crypto = require('crypto');
+var signature = crypto.createHmac('sha256', secretKey)
     .update(rawSignature)
-    .digest("hex");
-  console.log("--------------------SIGNATURE----------------");
-  console.log(signature);
+    .digest('hex');
+console.log("--------------------SIGNATURE----------------")
+console.log(signature)
 
-  //json object send to MoMo endpoint
-  const requestBody = JSON.stringify({
-    partnerCode: partnerCode,
-    partnerName: "Test",
-    storeId: "MomoTestStore",
-    requestId: requestId,
-    amount: amount,
-    orderId: orderId,
-    orderInfo: orderInfo,
-    redirectUrl: redirectUrl,
-    ipnUrl: ipnUrl,
-    lang: lang,
+//json object send to MoMo endpoint
+const requestBody = JSON.stringify({
+    partnerCode : partnerCode,
+    partnerName : "Test",
+    storeId : "MomoTestStore",
+    requestId : requestId,
+    amount : amount,
+    orderId : orderId,
+    orderInfo : orderInfo,
+    redirectUrl : redirectUrl,
+    ipnUrl : ipnUrl,
+    lang : lang,
     requestType: requestType,
     autoCapture: autoCapture,
-    extraData: extraData,
+    extraData : extraData,
     orderGroupId: orderGroupId,
-    signature: signature,
-  });
+    signature : signature
+});
+
   //option for axios
   const options = {
     method: "POST",
@@ -209,12 +207,9 @@ app.post("/payment", async (req, res) => {
 app.post("/callback", async (req, res) => {
   console.log("callback: ");
   console.log(res.body);
-  
+
   return res.status(200).json(req.body);
 });
-
-
-
 
 app.post("/transaction-status", async (req, res) => {
   const { orderId } = req.body;
@@ -233,5 +228,4 @@ app.post("/transaction-status", async (req, res) => {
     signature,
     lang: "vi",
   });
-
 });
